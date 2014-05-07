46272fafab4df5b6a61416e1b837f3d49937f679
Jean Arnaud <Jean Arnaud@10.0.5.109>
2014-05-07 16:42:13 +0200

    Updated model

--- test/src/main/java/test/model/Locomotive.java
+++ test/src/main/java/test/model/Locomotive.java
@@ -4,4 +4,16 @@ public class Locomotive extends RailwayVehicle {
 
 	private int power;
 
+	public Locomotive(int power) {
+		this.power = power;
+	}
+
+	public int getPower() {
+		return power;
+	}
+
+	public void setPower(int power) {
+		this.power = power;
+	}
+
 }
